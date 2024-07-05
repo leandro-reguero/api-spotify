b6ebfbc7c118c3425dbd679fae9cd5493b673881
leandro-reguero <lreguerolazaro@gmail.com>
2024-07-05 17:15:37 +0200

    proyecto iniciado, sin exito la conexion

--- src/app.py
+++ src/app.py
@@ -2,6 +2,17 @@ import os
 import pandas as pd
 import seaborn as sns
 from dotenv import load_dotenv
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
 
 # load the .env file variables
 load_dotenv()
+
+client_id = os.environ.get("CLIENT_ID")
+client_secret = os.environ.get("CLIENT_SECRET")
+
+
+spotify = spotipy.Spotify()
+
+lean_uri = 'spotify:artist:5Mtx4kiMwPhSbwVPVVR67g'
+
